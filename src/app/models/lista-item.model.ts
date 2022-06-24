6cf4647af521df6f381bc81b3c4b696acfab2791
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-06-24 16:15:44 -0500

    servicios

---
+++ src/app/models/lista-item.model.ts
@@ -0,0 +1,12 @@
+
+
+export class ListaItem {
+
+  descripcion1: string;
+  completado: boolean;
+
+  constructor(descripcion: string) {
+    this.descripcion1 = descripcion;
+    this.completado = false;
+  }
+}
